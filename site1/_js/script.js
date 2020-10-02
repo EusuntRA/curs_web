43f06e6f996827ecec40df11acd242074d7c73d1
Arty Rotaru <eusuntra@gmail.com>
2020-10-02 20:30:32 +0300

    Cursul web

---
+++ site1/_js/script.js
@@ -0,0 +1,4 @@
+
+		function butonel(){
+			alert ("Rău ai făcut, acum știu toate parolele tale");
+		}
---
+++ site2/frag/footer.php
@@ -0,0 +1,27 @@
+<?php
+echo '<div class="container">
+            <div class="row">
+                <div class="col-12 col-md-3 pt-5">
+                    2020 @ Toate drepturile furate
+                    <label for="mail"></label><input type="mail" name="mail" placeholder="Abonează-te">
+                    <label for="submit"></label><input type="submit" name="submit" class="footerbtn">
+                </div>
+                <div class="col-12 col-md-5">
+                    <div class="fhr"></div>
+                    <article class="certificat mb-5 pt-4    "><img src="/_img/certificatcisco.png"class="float-left pr-3"><p><span>CERTIFIED</span><br>Unified Modeling Language Professional 2™</p></article><div class="br10"></div>
+                    <article class="certificat mb-5"><img src="/_img/certificatuml.png"class="float-left pr-3"><p><span>CERTIFIED</span><br>Unified Modeling Language Professional 2™</p></article><div class="br10"></div>
+                    <article class="certificat mb-5"><img src="/_img/certificatibm.png"class="float-left pr-3"><p><span>CERTIFIED</span><br>Unified Modeling Language Professional 2™</p></article>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="fhr"></div>
+                    <div id="social">
+                        <img src="/_img/logo.png"><br>
+                        <a href="https://www.facebook.com/" target="_blanck"><i class="fab fa-facebook" style="color: #1975ED"></i></a>
+                        <a href="https://www.instagram.com/" target="_blanck"><i class="fab fa-instagram"></i></a>
+                        <a href="https://twitter.com/" target="_blanck"><i class="fab fa-twitter" style="color: #1C9DEB"></i></a>
+                        <a href="https://www.youtube.com/" target="_blanck"><i class="fab fa-youtube" style="color: red;"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>';
+?>
